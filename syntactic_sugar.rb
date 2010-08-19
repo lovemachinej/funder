3763870508d095b5919b29abd4178f13c0cb7791
d0c.s4vage <d0c.s4vage@42a6e906-0ded-82c0-aa8b-fa6bfddfe034>
2010-08-19 03:09:17 +0000

    [ issue 5 ] some more sugar

--- syntactic_sugar.rb
+++ syntactic_sugar.rb
@@ -21,10 +21,16 @@ module SyntacticSugar
 		pack = 'v' unless $BIG_ENDIAN
 		field(name, Int, value, {:p=>pack}.merge(options))
 	end
+	def short(name, value=nil, options={})
+		int16(name, value, options)
+	end
 	def int32(name, value=nil, options={})
 		pack = 'N'
 		pack = 'V' unless $BIG_ENDIAN
 		field(name, Int, value, {:p=>pack}.merge(options))
+	end
+	def long(name, value=nil, options={})
+		int32(name, value, options)
 	end
 	def double(name, value=nil, options={})
 		pack = 'G'
@@ -39,12 +45,16 @@ module SyntacticSugar
 
 	# string sugar
 
+	def str(name, value=nil, options={})
+		field(name, Str, value, options)
+	end
 	def char(name, value=nil, options={})
 		field(name, Str, value, {:min=>1, :max=>1}.merge(options))
 	end
 	def base64_str(name, value=nil, options={})
 		field(name, Str, value, {:action=>action(Base64Encode)}.merge(options))
 	end
+	# note - an extra byte will be added to this str (keep in mind for null-terminated lengths)
 	def c_str(name, value=nil, options={})
 		field(name, Str, value, {:action=>action(NullTerminate)}.merge(options))
 	end
